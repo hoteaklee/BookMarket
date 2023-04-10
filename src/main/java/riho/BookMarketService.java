282f9a36ab1cfc706d11c2c7ecb8b372aa30fe6f
hoteaklee <cc18ht@naver.com>
2023-04-10 15:42:26 +0900

    도서 쇼핑몰 예제 v4 Book 클래스 작성 도서정보 등록 및 조회

--- src/main/java/riho/BookMarketService.java
+++ src/main/java/riho/BookMarketService.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 public class BookMarketService {
     private Scanner sc = null;  // 기능을 수행하기 위한 변수 (값을 넣는것은 아님)
     private MemberShip m = null;    //회원정보 저장용
+    private Book[] books = null;    // 도서정보 저장용(배열로 저장)
+
     public  BookMarketService() {
         sc = new Scanner(System.in);
+        registerBooks();    // 도서 생성
     }
 
     public String displayMenu(){   // bookmatket 메뉴 작성
@@ -35,12 +38,16 @@ public class BookMarketService {
         switch (menu){
             case "1":
                 System.out.println("\n고객정보 확인");
-                 System.out.println(m);
+                System.out.println(m);
                 break;
             case "2":
                 System.out.println("\n장바구니 상품 목록\n"); break;
             case "3": System.out.println("\n장바구니 비우기\n"); break;
-            case "4": System.out.println("\n장바구니 상품 추가\n"); break;
+            case "4": System.out.println("\n장바구니 상품 추가\n");
+                    for (Book b : books){
+                        System.out.println(b);
+                    }
+                    break;
             case "5": System.out.println("\n주무수량 변경\n"); break;
             case "6": System.out.println("\n상품 삭제\n"); break;
             case "7": System.out.println("\n주문 영수증 출력\n"); break;
@@ -63,6 +70,18 @@ public class BookMarketService {
         String hp = sc.next();
 
         m = new MemberShip(230410,name,email, hp);
+    }
+
+    // 도서정보  자동등록
+    public void registerBooks(){
+        books = new Book[3];
+
+        books[0] = new Book("2304101234","JSP웹프로그래밍",27000,
+                "송미영","단계별로 쇼필몰구현","IT전문서","2018-10-08");
+        books[1] = new Book("2004104567","안드로이드 프로그래밍",33000,
+                "우재남","실습 멘토링 제공","IT전문서","2022-01-22");
+        books[2] = new Book("2304107891","스크래치",22000,
+                "고광일","사고력을 키우는 코딩","컴퓨터입문","2019-06-09");
     }
 
 }
