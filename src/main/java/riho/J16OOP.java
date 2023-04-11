a9236de8c0a90702abacbb87ca8c4b1b8e9aa5a4
hoteaklee <cc18ht@naver.com>
2023-04-11 11:17:37 +0900

    OOP 상속 2 메서드 오버라이딩

--- src/main/java/riho/J16OOP.java
+++ src/main/java/riho/J16OOP.java
@@ -12,6 +12,12 @@ public class J16OOP {
     // 통일성 유지, 다형성 구현 용이
 
     // 부모/상위/슈퍼 <=> 자식/하위/파생
+
+    // 메서드 재정의override
+    // 객체지향 중요 3대 개념중 다형성에 해당
+    // 상속관계에 있는 상위클래스의 메서드를
+    // 하위클래스에서 같은 이름의 메서드로 다시 작성하는 것을 의미
+    // 즉, 상속받은 메서드를 새로 정의해서 사용하는 것을 말함
     public static void main(String[] args) {
         SCV scv1 = new SCV();
         Marine marine1 = new Marine();
@@ -23,10 +29,14 @@ public class J16OOP {
 
         System.out.printf("마린 생명력 : %d\n",marine1.life);
         marine1.attack();
+        marine1.useStimpack();
         System.out.printf("파이어벳 생명력 : %d\n",firebat1.life);
         firebat1.attack();
+        firebat1.useStimpack();
         System.out.printf("메딕 생명력 : %d\n",medic1.life);
-        medic1.attack();
+        medic1.useHeal();
+        medic1.useRestoration();
+        medic1.useOpticalFlare();
     }
 }
 
@@ -56,6 +66,14 @@ class SCV extends Unit{
         // super : 보무클래스의 생성자를 호출하는 특수한 키워드
         super(60, 5, 2.3444, 7, 20);
     }
+    @Override   //@ 는 annotation주석 (컴파일러만 볼수 있는 메모)
+    protected void attack() {
+        System.out.printf("융합 절단기(%d)로 공격중...\n",power);
+    }
+
+    protected void collect(){
+        System.out.println("미레랄이나 개스핀 가스를 캐는중...");
+    }
 
 }
 
@@ -63,17 +81,43 @@ class Marine extends Unit{
     public Marine(){
         super(40,6,1.875,7,24);
     }
+
+    @Override
+    protected void attack() {
+        System.out.printf("가우스 소총(%d)으로 공격중...\n",power);
+    }
+
+    protected void useStimpack(){
+        System.out.printf("전투자극제 사용 : 공격력이 %.1f로 증가, 이동속도%.1f로 증가\n",power*1.72,move*1.5);
+    }
 }
 
 class Firebat extends Unit{
     public Firebat(){
         super(50,8*2,1.875,7,24);
     }
+    @Override
+    protected void attack() {
+        System.out.printf("화염방사기(%d)로 공격중...\n",power);
+    }
 
+    protected void useStimpack(){
+        System.out.printf("전투자극제 사용 : 공격력이 %.1f로 증가, 이동속도%.1f로 증가\n",power*1.96,move*1.5);
+    }
 }
 
 class Medic extends Unit{
     public Medic(){
         super(60,0,1.875,9,30);
+    }
+    protected void useHeal(){
+        System.out.println("대상 유닛의 체력을 초당 5.86만큼 회복시킴...");
+    }
+    protected void useRestoration(){
+        System.out.println("대상 유닛에 적용된 행동제약을 해제시킴..");
+    }
+
+    protected void useOpticalFlare(){
+        System.out.println("대상 유닛의 시야를 1로 고정시킴...");
     }
 }
