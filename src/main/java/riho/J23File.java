fb32df312cd39d836bded35a69c54a0d78d88e39
hoteaklee <cc18ht@naver.com>
2023-04-13 15:44:27 +0900

    파일입출력에 finally 예외처리 블럭 추가

--- src/main/java/riho/J23File.java
+++ src/main/java/riho/J23File.java
@@ -41,23 +41,27 @@ public class J23File {
         String fname = "c:/Java/info.txt";
 
         // 파일 쓰기(문자 출력스트림) 객체 생성
+        FileWriter fw = null;
         try {
-            FileWriter fw = new FileWriter(fname);
+            fw = new FileWriter(fname);
 
             // 파일에 내용 쓰기 : write(내용)
             fw.write(name + "\n");
             fw.write(height + "\n");
             fw.write(age + "\n");
 
-            // 파일쓰기가 끝나면 작업 종료
-            fw.close();
         }catch (Exception ex){
             ex.getMessage();
+        } finally {
+            // 파일쓰기가 끝나면 작업 종료
+            if (fw != null) try {fw.close();} catch (Exception ex){} //정상실행 되는 안되든 무조전 클로즈하기위해서
         }
+
         //파일에 저장된 데이터 화면에 출려 1 - 문자 하나씩 읽음
         // 파일읽기(문자 입력스트림) 객체 생성
+        FileReader fr = null;
         try {
-            FileReader fr = new FileReader(fname);
+            fr = new FileReader(fname);
 
             int ch = 0;
             // 파일에서 하나의 문자를 읽어옴 -fr.read()
@@ -66,26 +70,31 @@ public class J23File {
                 //문자로 변환해서 출력 (char)
                 System.out.print((char) ch);
             }
-            fr.close();
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
+        } finally {
+            if (fr != null)
+                try {fw.close();} catch (Exception ex){}
         }
 
         //파일에 저장된 데이터 화면에 출려 2 - 한 행씩 읽음
         // bufferedReader를 이용하면 입력속도를 높일수 있음
+        BufferedReader br = null;
         try {
-            FileReader fr = new FileReader(fname);
-            BufferedReader br = new BufferedReader(fr);
+            fr = new FileReader(fname);
+            br = new BufferedReader(fr);
 
             while (br.ready()){ //읽어올 데이터가 있는지 확인
                 // 만일, 있다면 한 행씩 읽어서 화면에 출력
                 System.out.println( br.readLine() );
             }
 
-            br.close(); // 닫을때는 역순으로 닫음 br fr 순으로
-            fr.close();
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
+        } finally {
+            // 파일쓰기가 끝나면 작업 종료
+            if (fw != null) try {br.close();} catch (Exception ex){} //정상실행 되는 안되든 무조전 클로즈하기위해서
+            if (fw != null) try {fr.close();} catch (Exception ex){}
         }
 
     }
